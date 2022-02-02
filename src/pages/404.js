0b02fa5bae6931a46e4e773c23f9ed27f8588164
Lucas Yamamoto <lucasayb97@gmail.com>
2022-02-02 20:24:12 -0300

    adds transition

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,9 @@ import * as React from "react"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import sorryGIF from '../gifs/sorry.gif'
-import { Link } from 'gatsby'
+import AniLink from "gatsby-plugin-transition-link/AniLink";
 import * as S from '../components/NotFound/styled'
+import getThemeColor from '../utils/getThemeColor';
 
 const NotFoundPage = () => (
   <Layout>
@@ -15,7 +16,7 @@ const NotFoundPage = () => (
       </S.SorryGifWrapper>
       <S.TitleNotFound>404</S.TitleNotFound>
       <S.SubTitleNotFound>Desculpa :( Página não encontrada</S.SubTitleNotFound>
-      <S.TextNotFound>A página que você buscou não pôde ser encontrada. Utilize minha excelente busca pra encontrar o que você desejada clicando <Link to="/search">aqui</Link></S.TextNotFound>
+      <S.TextNotFound>A página que você buscou não pôde ser encontrada. Utilize minha excelente busca da Algolia pra encontrar o que você deseja clicando <AniLink title="Ir para a busca" bg={getThemeColor()} duration={0.6} cover direction="top" to="/search">aqui</AniLink></S.TextNotFound>
     </S.NotFoundWrapper>
   </Layout>
 )
