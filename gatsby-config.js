534b7adad3783b7a4b2cba76fd61b0e1b221a56e
Lucas Yamamoto <lucasayb97@gmail.com>
2022-02-02 08:12:31 -0300

    adds algolia logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,10 +105,17 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: process.env.GATSBY_GTM_ID,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     'gatsby-redirect-from',
     'gatsby-plugin-meta-redirect' // make sure this is always the last one
   ],
+  
 }
