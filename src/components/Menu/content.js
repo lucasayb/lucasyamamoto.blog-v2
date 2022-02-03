22d06aa477892801539d0b6e108d3353d34e53c4
Lucas Yamamoto <lucasayb97@gmail.com>
2022-02-03 04:35:38 -0300

    hide future links

--- src/components/Menu/content.js
+++ src/components/Menu/content.js
@@ -24,8 +24,8 @@ export const socialLinks = [
 export const navLinks = [
   { path: "/", name: "Home" },
   { path: "/about", name: "Sobre" },
-  { path: "/my-events", name: "Eventos que participei" },
-  { path: "/courses", name: "Cursos" },
-  { path: "/my-trips", name: "Viagens" },
-  { path: "/my-books", name: "Livros que li" },
+  // { path: "/my-events", name: "Eventos que participei" },
+  // { path: "/courses", name: "Cursos" },
+  // { path: "/my-trips", name: "Viagens" },
+  // { path: "/my-books", name: "Livros que li" },
 ]
