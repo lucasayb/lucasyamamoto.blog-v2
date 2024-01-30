454e979b3a9a37ca3ad5644b3de724a6df12c048
Lucas Yamamoto <17356081+lucasayb@users.noreply.github.com>
2024-01-30 01:13:41 -0300

    changes to lowercase

--- src/components/Post/styled.js
+++ src/components/Post/styled.js
@@ -26,6 +26,7 @@ export const PostDate = styled.time`
   color: var(--primary-text-color);
   font-size: 0.875rem;
   font-weight: 100;
+  text-transform: uppercase;
 `
 
 export const PostDescription = styled.h2`
