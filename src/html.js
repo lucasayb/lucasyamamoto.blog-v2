6f4ac328df8f25d846e0efd68fa9219b99befe94
Lucas Yamamoto <17356081+lucasayb@users.noreply.github.com>
2024-02-05 13:25:16 -0300

    adds noindex

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta name="robots" content="noindex"/ >
         {props.headComponents}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
